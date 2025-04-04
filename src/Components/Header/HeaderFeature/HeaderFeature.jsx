139cd95fe18a0a582fd70b5ec8fa0c54f53358ab
Zo <161946731+zozo0329@users.noreply.github.com>
2025-04-04 11:57:44 +0800

    check in

--- src/Components/Header/HeaderFeature/HeaderFeature.jsx
+++ src/Components/Header/HeaderFeature/HeaderFeature.jsx
@@ -3,18 +3,20 @@ import "./HeaderFeature.css";
 const HeaderFeature = () => {
   return (
     <>
-      <div className="Navbar">
-        <div className="Popular-now">
-          <button>Popular Now</button>
-        </div>
-        <div className="Plans">
-          <button>Plans</button>
-        </div>
-        <div className="Reasons-to-join">
-          <button>Reasons to Join</button>
-        </div>
-        <div className="FAQ">
-          <button>FAQ</button>
+      <div className="Navbar_HERO">
+        <div className="Navbar">
+          <div className="Popular-now">
+            <button>Popular Now</button>
+          </div>
+          <div className="Plans">
+            <button>Plans</button>
+          </div>
+          <div className="Reasons-to-join">
+            <button>Reasons to Join</button>
+          </div>
+          <div className="FAQ">
+            <button>FAQ</button>
+          </div>
         </div>
       </div>
     </>
